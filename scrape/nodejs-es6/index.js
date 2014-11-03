7b1413d228641e984c62b668051756e6d05ff5de
Daniel Lauzon <daniel.lauzon@gmail.com>
2014-11-03 04:34:34 -0500

    Refactor request factories (use lodash)

--- scrape/nodejs-es6/index.js
+++ scrape/nodejs-es6/index.js
@@ -1,9 +1,15 @@
 "use strict";
-
-var credentials = require('./credentials.json');
+// dependencies
 var Promise = require("bluebird");
 var rp = require('request-promise');
+var _ = require('lodash');
+
+// globals
+// external data for creds.
+var credentials = require('./credentials.json');
+//  jar singleton to persist cookies (for now)
 var jar = rp.jar();
+
 
 var baseUri = 'https://play.pocketcasts.com';
 var sign_in = '/users/sign_in';
@@ -12,6 +18,7 @@ var new_releases_episodes = '/web/episodes/new_releases_episodes.json';
 var in_progress_episodes = '/web/episodes/in_progress_episodes.json';
 var find_by_podcast = '/web/episodes/find_by_podcast.json';
 
+// Cookie helpers
 function getCookieValue(name) {
   var cookies = jar.getCookies(baseUri);
   // console.log('--=-=-= jar.getCookieString', jar.getCookieString('https://play.pocketcasts.com'));
@@ -31,33 +38,58 @@ function XSRF() {
   return decodeURIComponent(getCookieValue('XSRF-TOKEN'));
 }
 
+// Generate a new option base for request invocation (rp)
+// dependency on lodash's `_.merge` deep copy.
+function reqGen(path, options) {
+  return _.merge({
+    jar: jar,
+    uri: baseUri + path,
+  }, options);
+}
+
+function reqGenXSRF(path, options) {
+  var o = _.merge(reqGen(path, {
+    method: 'POST',
+    headers: {
+      'X-XSRF-TOKEN': XSRF()
+    }
+  }), options);
+  // console.log('*X*', o.headers['X-XSRF-TOKEN'] );
+  // console.log('*R*', JSON.stringify(o, function(key, value) {
+  //   return (key === "jar") ? undefined : value;
+  // }));
+  return o;
+}
+
+function reqJSON(path, params) {
+  return reqGenXSRF(path, {
+    headers: {
+      'Content-Type': 'application/json;charset=UTF-8'
+    },
+    json: params || {},
+  });
+}
+
 // Login process:
 // GET /users/sign_in, to get cookies (XSRF-TOKEN)
 // POST form to /users/sign_in, with authenticity_token and credentials
 //  Note: the POST returns a 302, which rejects the promise, 
 //  whereas a faled login returns the login page content again (200)
 //  the 302 response also has a new XSRF-TOKEN cookie
-rp({
-  jar: jar,
-  uri: baseUri + sign_in,
+rp(reqGen(sign_in, {
   resolveWithFullResponse: true
-})
+}))
   .then(function(response) {
-    // TODO clone credentials, and add authenticity_token
-    credentials.authenticity_token = XSRF();
+    var form = _.merge({
+      authenticity_token: XSRF()
+    }, credentials);
 
     // now do a form post for login, expect a 302, which is not followed for POST.        
     // unless followAllRedirects: true, but that only follows back to / and causes an extra fetch
     return new Promise(function(resolve, reject) {
-      rp({
-        jar: jar,
-        uri: baseUri + sign_in,
-        method: 'POST',
-        headers: {
-          'X-XSRF-TOKEN': XSRF()
-        },
-        form: credentials,
-      }).then(function(response) {
+      rp(reqGenXSRF(sign_in, {
+        form: form
+      })).then(function(response) {
         console.log('response OK, expecting 302, reject.', response);
         reject('Login NOT OK');
       }).catch(function(error) { // error: {error:,options,response,statusCode}
@@ -72,98 +104,45 @@ rp({
     });
   })
   .then(function() {
-    console.log('XSRF', XSRF());
-  })
-  .then(function() {
-    return rp({
-      jar: jar,
-      uri: baseUri + new_releases_episodes,
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json;charset=UTF-8',
-        'X-XSRF-TOKEN': XSRF()
-      },
-      json: {},
-      // body:'{}',
-      // resolveWithFullResponse: true
-    }).then(function(response) {
+    return rp(reqJSON(new_releases_episodes)).then(function(response) {
       console.log('XSRF', XSRF());
       console.log('new_releases_episodes:', response.episodes.length);
       return response;
     });
   })
   .then(function() {
-    return rp({
-      jar: jar,
-      uri: baseUri + in_progress_episodes,
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json;charset=UTF-8',
-        'X-XSRF-TOKEN': XSRF()
-      },
-      json: {},
-    }).then(function(response) {
+    return rp(reqJSON(in_progress_episodes)).then(function(response) {
       console.log('XSRF', XSRF());
       console.log('in_progress_episodes:', response.episodes.length);
       return response;
     });
   })
   .then(function() {
-    return rp({
-      jar: jar,
-      uri: baseUri + podcasts_all,
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json;charset=UTF-8',
-        'X-XSRF-TOKEN': XSRF()
-      },
-      json: {},
-    }).then(function(response) {
+    return rp(reqJSON(podcasts_all)).then(function(response) {
       console.log('XSRF', XSRF());
       console.log('podcasts_all:', response.podcasts.length);
       return response;
     });
   })
   .then(function() {
-    // adventures in angluar
-    var uuid = "80931490-01be-0132-a0fb-5f4c86fd3263";
-    return rp({
-      jar: jar,
-      uri: baseUri + find_by_podcast,
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json;charset=UTF-8',
-        'X-XSRF-TOKEN': XSRF()
-      },
-      json: {
-        uuid: uuid,
-        page: 1
-      },
-    }).then(function(response) {
+    var json = {
+      uuid: "80931490-01be-0132-a0fb-5f4c86fd3263", // adventures in angluar
+      page: 1
+    }
+    return rp(reqJSON(find_by_podcast, json)).then(function(response) {
       console.log('XSRF', XSRF());
-      console.log('find_by_podcast:', response);
+      console.log('find_by_podcast:AiA', JSON.stringify(response,null,2));
       return response;
     });
   })
   .then(function() {
-    // History of Rome
-    var uuid = "e4b6efd0-0424-012e-f9a0-00163e1b201c";
-
-    return rp({
-      jar: jar,
-      uri: baseUri + find_by_podcast,
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json;charset=UTF-8',
-        'X-XSRF-TOKEN': XSRF()
-      },
-      json: {
-        uuid: uuid,
-        page: 16
-      },
-    }).then(function(response) {
+    var json = {
+      uuid: "e4b6efd0-0424-012e-f9a0-00163e1b201c", // History of Rome
+      page: 16
+    }
+    return rp(reqJSON(find_by_podcast, json)).then(function(response) {
       console.log('XSRF', XSRF());
-      console.log('find_by_podcast: HoR', response);
+      console.log('find_by_podcast:HoR', JSON.stringify(response,null,2));
       return response;
     });
   })

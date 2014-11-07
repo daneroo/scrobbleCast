9da2ff9b7ff58a79fc25229c19cdf9bc5d732cc4
Daniel Lauzon <daniel.lauzon@gmail.com>
2014-11-07 04:25:54 -0500

    Start theming

--- web/app.js
+++ web/app.js
@@ -1,4 +1,4 @@
-angular.module('scrobbleCast', [/*'ngMaterial','ui.bootstrap','ui.utils'*/,'ui.router',/*'ngAria'*/,'ngAnimate']);
+angular.module('scrobbleCast', ['ngMaterial',/*'ui.bootstrap','ui.utils'*/,'ui.router','ngAria','ngAnimate']);
 
 angular.module('scrobbleCast').config(function($stateProvider, $urlRouterProvider) {
   console.log('scrobbleCast.config');

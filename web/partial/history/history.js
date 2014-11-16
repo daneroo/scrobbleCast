cc620b21a411db80c60c3ba21d15eaac66ef090b
Daniel Lauzon <daniel.lauzon@gmail.com>
2014-11-15 21:54:44 -0500

    history by day

--- web/partial/history/history.js
+++ web/partial/history/history.js
@@ -1,33 +1,59 @@
 angular.module('scrobbleCast').controller('HistoryCtrl', function($scope, scrobbleSvc) {
   "use strict";
-  var img = '//placehold.it/64x64&text=SC';
-
   // export moment.js to view
   $scope.moment = moment;
 
+  // change in changes|filter:changeFilter
+  $scope.playFilter = {
+    // changes:{length:true}
+  };
+
+  // change in changes|filter:changeFilter
   $scope.changeFilter = {
     op: 'chg',
     // key: 'duration'
     key: 'played_up_to'
   };
 
+  $scope.lookupThumb = function(podcast_uuid) {
+    var defaultUrl = '/images/podcast.jpg';
+    var podcast = $scope.podcastsByUuid[podcast_uuid];
+    // the groupBy makes arrays:
+    podcast = (podcast)?podcast[0]:{};
+    return podcast.thumbnail_url || defaultUrl;
+  };
+
+  $scope.podcastsByUuid = {};
   $scope.history = [];
   $scope.days = [];
   $scope.historyByDay = {};
 
-  scrobbleSvc.history()
+  // handle result
+  function handleHistory(result) {
+    var history = _.sortBy(result, ['stamp']).reverse();
+    // group by day
+    var historyByDay = _.groupBy(result, function(ev) {
+      return moment(ev.stamp).format('YYYY-MM-DD');
+    });
+    var days = Object.keys(historyByDay);
+    days = _.chain(days).sort().reverse().value();
+
+    // expose on $scope
+    $scope.history = history;
+    $scope.historyByDay = historyByDay;
+    $scope.days = days;
+    return history;
+  }
+
+  scrobbleSvc.podcasts()
     .then(function(result) {
-      return _.sortBy(result, ['stamp']).reverse();
+      var podcastsByUuid = _.groupBy(result, 'uuid');
+      $scope.podcastsByUuid = podcastsByUuid;
+      console.log('podcastsByUuid', podcastsByUuid);
     })
-    .then(function(result) {
-      $scope.history = result;
-      $scope.historyByDay = _.groupBy(result, function(ev) {
-        return moment(ev.stamp).format('YYYY-MM-DD');
-      });
-      var days = Object.keys($scope.historyByDay);
-      days = _.chain(days).sort().reverse().value();
-      $scope.days = days;
-      console.log('hbd[0]',$scope.historyByDay[days[0]]);
+    .then(function() {
+      return scrobbleSvc.history()
+        .then(handleHistory);
     })
     .catch(function(error) {
       console.error('error', error);

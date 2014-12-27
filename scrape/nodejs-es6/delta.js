6ee376fad6722de48364c9e6dc9b9099fc589b39
Daniel Lauzon <daniel.lauzon@gmail.com>
2014-12-27 13:12:42 -0500

    copy dedup.js back to delta.js

--- scrape/nodejs-es6/delta.js
+++ scrape/nodejs-es6/delta.js
@@ -14,80 +14,8 @@ var srcFile = require('./lib/source/file');
 var sinkFile = require('./lib/sink/file');
 var delta = require('./lib/delta');
 
-// Target Key (path) definitions
-// - /podcast/<podast_uuid>/<stamp>/01-podcasts <-source type (url)
-// - /podcast/<podast_uuid>/episode/<episode_uuid>/<stamp>/0[234]-type <-source type 
-// - /episode/<episode_uuid>/<stamp>/0[234]-type <-source type
-
-// example file input patterns
-// data/byDate/2014-11-07T08:34:00Z/02-podcasts/2cfd8eb0-58b1-012f-101d-525400c11844.json
-// data/byDate/2014-11-05T07:40:00Z/03-new_releases.json
-// data/byDate/2014-11-05T07:40:00Z/04-in_progress.json
-
-// @param file(name)
-// @param thingsToMerge: [{podcast|episode}]
-// return  [{key:{type,[podcast_uuid],uuid,stamp,sourceType} values:[things]}]
-function readByDate(file) {
-  var thingsToMerge = srcFile.loadJSON(file);
-  if (thingsToMerge.length === 0) {
-    // throw new Error('readByDate: no things to split by keys: ' + file);
-    return [];
-  }
-
-  var stamp = utils.stampFromFile(file);
-  // match the sourceType and optionally the podcast_uuid for 02-podcasts (old)
-  var match = file.match(/(01-podcasts|02-podcasts|03-new_releases|04-in_progress)(\/(.*))?\.json/);
-  var sourceType = match[1];
-  var podcast_uuid = match[3];
-  var type = (sourceType === '01-podcasts') ? 'podcast' : 'episode';
-
-  // extra assert (02- fix)
-  if (sourceType === '02-podcasts' && !podcast_uuid) {
-    throw (new Error('readByDate: no podcast_uuid for 02-podcasts: ' + file));
-  }
-  var keyedThings = thingsToMerge.map(function(thing) {
-
-    // assertions
-    if (!thing.uuid) {
-      throw (new Error('readByDate: no uuid in thing!'))
-    }
-    if (type === 'episode' && !thing.podcast_uuid && !podcast_uuid) {
-      throw (new Error('readByDate: no podcast_uuid for file:' + file));
-    }
-    if (!thing.title) { // just checking because we are adding to key - for tracing
-      throw new Error('readByDate missing title' + JSON.stringify(keyedThings));
-    }
-
-    // 02-fix
-    if (type === 'episode' && !thing.podcast_uuid) {
-      thing.podcast_uuid = podcast_uuid;
-    }
-
-    // mapping the key - try to preserve order...
-    var key = {
-      type: type,
-      uuid: thing.uuid,
-      stamp: stamp,
-      sourceType: sourceType,
-    };
-    if (key.type === 'episode') {
-      key.podcast_uuid = thing.podcast_uuid;
-    }
-
-    // optional below
-    // decorate with source file/title - for tracing/debug
-    _.merge(key, {
-      source: file,
-      title: thing.title
-    });
-
-    return {
-      key: key,
-      value: thing
-    };
-  });
-  return keyedThings;
-}
+// globals
+var allCredentials = require('./credentials.json');
 
 // mv the file to dedup folder
 // should be async
@@ -99,22 +27,22 @@ function dedup(file) {
   var oldFilename = path.join(dataDirname, file);
   var oldDir = path.dirname(oldFilename);
 
-  var nuFilename = path.join(dataDirname, 'dedup',file);
+  var nuFilename = path.join(dataDirname, 'dedup', file);
   var nuDir = path.dirname(nuFilename)
 
   mkdirp.sync(nuDir);
-  fs.renameSync(oldFilename,nuFilename);
-  console.log('-exec fs.renameSync(%s, %s)',oldFilename,nuFilename);
+  fs.renameSync(oldFilename, nuFilename);
+  console.log('-exec fs.renameSync(%s, %s)', oldFilename, nuFilename);
 
   // now prune oldDir (and parent) - if empty
   try {
     fs.rmdirSync(oldDir);
     // only prints if emtpy - no error
-    console.log('-exec fs.rmdirSync(%s)',oldDir);
+    console.log('-exec fs.rmdirSync(%s)', oldDir);
     // and parent - unless oldDir already not empty...
     fs.rmdirSync(path.dirname(oldDir));
     // only prints if emtpy - no error
-    console.log('-exec fs.rmdirSync(%s)',path.dirname(oldDir));
+    console.log('-exec fs.rmdirSync(%s)', path.dirname(oldDir));
   } catch (e) {
     // code: 'ENOTEMPTY'
     // console.log('rmdir error:',e);
@@ -123,85 +51,97 @@ function dedup(file) {
   }
 }
 
-// srcFile.find('byDate/**/*.json')
-srcFile.findByDate()
-  .then(function(stamps) {
-    utils.logStamp('Starting:Delta ');
-    // console.log('stamps', stamps);
-    console.log('-|stamps|', stamps.length);
-    // stamps = stamps.slice(0, 3000);
-    console.log('+|stamps|', stamps.length);
+utils.serialPromiseChainMap(allCredentials, function(credentials) {
+  utils.logStamp('Starting job for ' + credentials.name);
 
-    var uuidProperty = 'uuid'; // common to all: podcasts/episodes
-    var podcastHistory = new delta.AccumulatorByUuid();
-    var episodeHistory = new delta.AccumulatorByUuid();
+  return srcFile.findByUserStamp(credentials.name)
+    .then(function(stamps) {
+      utils.logStamp('Starting:Dedup for ' + credentials.name);
+      console.log('-|stamps|', stamps.length);
 
-    var partCount = 0;
-    var fileCount = 0;
-    var dedupPartCount = 0;
-    var dedupFileCount = 0;
+      var uuidProperty = 'uuid'; // common to all: podcasts/episodes
+      var podcastHistory = new delta.AccumulatorByUuid();
+      var episodeHistory = new delta.AccumulatorByUuid();
 
-    // should have a version without aggregation
-    utils.serialPromiseChainMap(stamps, function(stamp) {
-        console.log('--iteration stamp:', stamp);
-        return srcFile.find(path.join('byDate', stamp, '**/*.json'))
-          .then(function(files) {
+      var partCount = 0;
+      var fileCount = 0;
+      var dedupPartCount = 0;
+      var dedupFileCount = 0;
 
-            files.forEach(function(file) {
+      // should have a version without aggregation
+      return utils.serialPromiseChainMap(stamps, function(stamp) {
+          console.log('--iteration stamp:', credentials.name, stamp);
+          return srcFile.find(path.join('byUserStamp', credentials.name, stamp, '**/*.json'))
+            .then(function(files) {
 
-              // console.log('---file:', file);
-              var keyedThings = readByDate(file);
+              files.forEach(function(file) {
 
-              fileCount++;
-              var fileHasChanges = false;
-              keyedThings.forEach(function(keyedThing) {
-                partCount++;
-                // watch this overwrite (could do my own mergeMany...)
+                // console.log('---file:', file);
+                var items = srcFile.loadJSON(file);
+                var redux = [];
 
-                // Normalize values (bool/null) (no cloning...)
-                keyedThing.value = delta.normalize(keyedThing.value);
+                fileCount++;
+                var fileHasChanges = false;
+                items.forEach(function(item) {
+                  partCount++;
 
-                var keyedThings = [keyedThing];
-                var changeCount = 0;
-                if (file.match(/01-/)) {
-                  // console.log('|podcasts|', thingsToMerge.length,file);
-                  changeCount += podcastHistory.mergeMany(keyedThings);
-                } else {
-                  // console.log('|episodes|', thingsToMerge.length,file);
-                  changeCount += episodeHistory.mergeMany(keyedThings);
+                  // passed item is cloned, and normalized in accumulator
+
+                  var changeCount = 0;
+                  if (item.__type === 'episode') {
+                    // console.log('|episode|', thingsToMerge.length,file);
+                    changeCount += episodeHistory.merge(item);
+                  } else {
+                    // console.log('|podcasts|', thingsToMerge.length,file);
+                    changeCount += podcastHistory.merge(item);
+                  }
+
+                  if (changeCount > 0) {
+                    fileHasChanges = true;
+                    redux.push(item);
+                    console.log('---|Δ|', changeCount, item.title);
+                    // TODO append to redux
+                  } else {
+                    dedupPartCount++;
+                  }
+
+                });
+
+
+                // fileHasChanges === redux.length>0
+                if (!fileHasChanges) {
+                  dedupFileCount++;
+                  dedup(file);
+                  // console.log('---dedup: file %d/%d  part %d/%d', dedupFileCount, fileCount, dedupPartCount, partCount);
                 }
-                if (changeCount > 0) {
-                  fileHasChanges = true;
-                  console.log('---|Δ|', changeCount, keyedThing.key.title);
-                } else {
-                  dedupPartCount++;
+                // else: fileHasHasganges===true or redux.length>0
+                if (redux.length > 0) {
+                  console.log('---redux: |parts|: %d file: %s', redux.length,file);
+                  var basepath = path.join(sinkFile.dataDirname,'redux');
+                  sinkFile.writeByUserStamp(redux,basepath);
                 }
+
               });
-              if (!fileHasChanges) {
-                dedupFileCount++;
-                dedup(file);
-                console.log('---dedup: file %d/%d  part %d/%d', dedupFileCount, fileCount, dedupPartCount, partCount);
-              }
-
             });
-          });
-      })
-      .then(function(dontCare) {
-        function sortAndSave(outfile, history) {
-          console.log('|' + outfile + '|=', _.size(history.accumulators));
-          // just write out the accumulators dictionary, it is the only attribute!
-          var sorted = _.sortBy(history.accumulators, 'lastUpdated').reverse();
-          fs.writeFileSync(outfile, JSON.stringify(sorted, null, 2));
-        }
-        sortAndSave('podcast-history.json', podcastHistory);
-        sortAndSave('episode-history.json', episodeHistory);
+        })
+        .then(function(dontCare) {
+          function sortAndSave(outfile, history) {
+            console.log('|' + outfile + '|=', _.size(history.accumulators));
+            // just write out the accumulators dictionary, it is the only attribute!
+            var sorted = _.sortBy(history.accumulators, 'lastUpdated').reverse();
+            fs.writeFileSync(outfile, JSON.stringify(sorted, null, 2));
+          }
+          sortAndSave('podcast-history-' + credentials.name + '.json', podcastHistory);
+          sortAndSave('episode-history-' + credentials.name + '.json', episodeHistory);
 
-        utils.logStamp('Done:Delta |e|:' + _.size(episodeHistory.accumulators));
-        return stamps;
-      });
+          console.log('Done:dedup[%s] |f|: %d/%d  |p|: %d/%d', credentials.name, dedupFileCount, fileCount, dedupPartCount, partCount);
+          utils.logStamp('Done:Dedup[' + credentials.name + '] |f|:' + fileCount + ' |p|:' + partCount);
+          return stamps;
+        });
 
-  })
-  .catch(function(error) {
-    console.error('Error:Delta', error);
-    utils.logStamp('Error:Delta ' + error);
-  });
+    })
+    .catch(function(error) {
+      console.error('Error:Dedup', error);
+      utils.logStamp('Error:Dedup ' + error);
+    });
+});

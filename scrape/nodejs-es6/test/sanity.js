62f94764e0e89482de2c872b61c1c48ff53b1070
Daniel Lauzon <daniel.lauzon@gmail.com>
2015-02-22 16:21:58 -0500

    add first test (sanity)

---
+++ scrape/nodejs-es6/test/sanity.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var assert = require('should');
+
+describe('pocketscrape', function () {
+
+  it('should be awesome', function () {
+    var thing = 'awesome'
+    thing.should.equal('awesome');
+  });
+
+});

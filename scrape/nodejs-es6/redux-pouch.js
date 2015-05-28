0326fd40761045f6b5a78e6f2c75bd6b82eba7d5
Daniel Lauzon <daniel.lauzon@gmail.com>
2015-05-28 03:19:49 -0400

    restore file to pouch: read hash back from db

--- scrape/nodejs-es6/redux-pouch.js
+++ scrape/nodejs-es6/redux-pouch.js
@@ -4,14 +4,26 @@
 
 // dependencies - core-public-internal
 var path = require('path');
+var util = require('util');
 var utils = require('./lib/utils');
 var Promise = require('bluebird');
 var PouchDB = require('pouchdb');
 var srcFile = require('./lib/source/file');
 
+// globals
+var allCredentials = require('./credentials.json');
 var db = new PouchDB('pouchdb');
 
-function showAll(msg) {
+function verbose(msg, thing) {
+  console.error(msg, thing);
+}
+
+function verboseErrorHandler(error) {
+  verbose('error', error);
+  throw (error);
+}
+
+function showAll() {
   return function() {
     return db.allDocs({
         include_docs: true
@@ -19,9 +31,10 @@ function showAll(msg) {
       .then(function(response) {
         response.rows.forEach(function(item) {
           var d = item.doc;
-          console.log(JSON.stringify({id:d._id,r:d._rev,t:d.item.title}));
+          delete d._rev;
+          console.log('-doc', JSON.stringify(d));
         });
-        console.log(msg,'total_rows',response.total_rows);
+        verbose('total_rows', response.total_rows);
         return response;
       });
   };
@@ -31,7 +44,9 @@ function create(item) {
   // with - or without stamp
   // var key = [item.__user, item.__stamp, item.__type, item.uuid].join('/');
   var key = [item.__user, item.__type, item.uuid].join('/');
-  item = {item:item};
+  item = {
+    item: item
+  };
   item._id = key;
   return db.get(key)
     .catch(function(error) {
@@ -40,14 +55,12 @@ function create(item) {
 }
 
 function save(doc) {
-  console.log('--saving', doc._id);
+  // verbose('--saving', doc._id);
   return db.put(doc)
-  .then(function(doc){
-    return doc;
-  })
-  .catch(function(error){
-    console.log('error',error);
-  });
+    .then(function(doc) {
+      return doc;
+    })
+    .catch(verboseErrorHandler);
 }
 
 function createAndUpdate(item) {
@@ -55,53 +68,49 @@ function createAndUpdate(item) {
     .then(save);
 }
 
-// globals
-var allCredentials = require('./credentials.json');
-
 utils.serialPromiseChainMap(allCredentials, function(credentials) {
-  utils.logStamp('Starting job for ' + credentials.name);
+    verbose('Starting job: ', credentials.name);
 
-  // var basepath = path.join(srcFile.dataDirname, 'redux');
-  var basepath = srcFile.dataDirname;
+    // var basepath = path.join(srcFile.dataDirname, 'redux');
+    var basepath = srcFile.dataDirname;
 
-  return srcFile.findByUserStamp(credentials.name, basepath)
-    .then(function(stamps) {
-      utils.logStamp('Starting:redux for ' + credentials.name);
-      console.log('-|stamps|', stamps.length);
+    return srcFile.findByUserStamp(credentials.name, basepath)
+      .then(function(stamps) {
+        verbose('-|stamps|', stamps.length);
 
-      var partCount = 0;
-      var fileCount = 0;
+        var partCount = 0;
+        var fileCount = 0;
 
-      // should have a version without aggregation
-      return utils.serialPromiseChainMap(stamps, function(stamp) {
-          console.log('--iteration stamp:', credentials.name, stamp);
-          return srcFile.find(path.join('byUserStamp', credentials.name, stamp, '**/*.json'))
-            .then(function(files) {
+        // should have a version without aggregation
+        return utils.serialPromiseChainMap(stamps, function(stamp) {
+            verbose('--iteration stamp:', [credentials.name, stamp]);
+            return srcFile.find(path.join('byUserStamp', credentials.name, stamp, '**/*.json'))
+              .then(function(files) {
 
-              return Promise.map(files, function(file) {
+                return Promise.map(files, function(file) {
 
-                console.log('---file:', file);
-                var items = srcFile.loadJSON(file);
+                  // verbose('---file:', file);
+                  var items = srcFile.loadJSON(file);
 
-                fileCount++;
-                return Promise.map(items, function(item) {
-                  partCount++;
-                  return createAndUpdate(item);
-                },{concurrency: 1});
+                  fileCount++;
+                  return Promise.map(items, function(item) {
+                    partCount++;
+                    return createAndUpdate(item);
+                  }, {
+                    concurrency: 1
+                  });
 
-              },{concurrency: 1});
-            });
-        })
-        .then(function(dontCare) {
-          console.log('Done:redux[%s] |f|: %d  |p|: %d', credentials.name, fileCount, partCount);
-          utils.logStamp('Done:redux[' + credentials.name + '] |f|:' + fileCount + ' |p|:' + partCount);
-          return stamps;
-        });
+                }, {
+                  concurrency: 1
+                });
+              });
+          })
+          .then(function(dontCare) {
+            verbose('Done job:', util.format('%s |f|: %d  |p|: %d', credentials.name, fileCount, partCount));
+            return stamps;
+          });
 
-    })
-    .catch(function(error) {
-      console.error('Error:Dedup', error);
-      utils.logStamp('Error:Dedup ' + error);
-    });
-})
-.then(showAll('after'));
+      })
+      .catch(verboseErrorHandler);
+  })
+  .then(showAll());

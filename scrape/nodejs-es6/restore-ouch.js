a673c7b5e746a3dd79728408e8977e8980b43630
Daniel Lauzon <daniel.lauzon@gmail.com>
2015-06-14 16:28:32 -0400

    Prepare restore-ouch for persisting accumulators

--- scrape/nodejs-es6/restore-ouch.js
+++ scrape/nodejs-es6/restore-ouch.js
@@ -6,6 +6,7 @@
 var _ = require('lodash');
 var PouchDB = require('pouchdb');
 var srcFile = require('./lib/source/file');
+var delta = require('./lib/delta');
 var ouch = require('./lib/ouch');
 
 // globals
@@ -15,14 +16,12 @@ var db = new PouchDB('pouchdb');
 // var db = new PouchDB('http://admin:supersecret@cantor:5984/scrobblecast');
 
 //  move to logging module (?loggly)
-function verbose(msg, thing) {
-  console.error(msg, thing);
-}
+var log = console.error;
 
 //  move to logging module (as Factory?)
 function verboseErrorHandler(shouldRethrow) {
   return function errorHandler(error) {
-    verbose('error', error);
+    log('error', error);
     if (shouldRethrow) {
       throw (error);
     }
@@ -34,7 +33,7 @@ function resetDB(dropAndCreate) {
   if (!dropAndCreate) {
     return db.info()
       .then(function(info) {
-        verbose('-=-= NOT DROPPING Database', info);
+        log('-=-= NOT DROPPING Database', info);
       });
   }
   // else drop and create : maybe not on couchDB (bulk delete all instead)
@@ -59,7 +58,7 @@ function showAll() {
           delete d._rev;
           console.log('-doc:', JSON.stringify(d));
         });
-        verbose('total_rows', response.total_rows);
+        log('total_rows', response.total_rows);
         return response;
       });
   };
@@ -69,31 +68,31 @@ function showAll() {
 function create(item) {
   ouch.normalize(item); // _id and meta
 
-  // verbose('--fetching', item._id);
+  // log('--fetching', item._id);
   return db.get(item._id)
     .then(function(doc) {
-      // verbose('--found:', [doc._id,doc._rev]);
+      // log('--found:', [doc._id,doc._rev]);
       var merged = _.merge({}, doc, item);
-      // verbose('--merged', [merged._id,merged._rev]);
+      // log('--merged', [merged._id,merged._rev]);
       return merged;
     })
     .catch(function(error) {
-      // verbose('--new!', item._id);
+      // log('--new!', item._id);
       return item;
     });
 }
 
 //  create or update
 function save(item) {
-  // verbose('--saving', item._id);
+  // log('--saving', item._id);
   return db.put(item)
     .then(function(doc) {
       item._rev = doc.rev;
-      // verbose('--saved', [item._id,item._rev]);
+      // log('--saved', [item._id,item._rev]);
       return item;
     })
     .catch(function(error) {
-      verbose('error:doc', [item._id,item._rev]);
+      log('error:doc', [item._id, item._rev]);
       throw (error);
     })
     .catch(verboseErrorHandler(true));
@@ -104,68 +103,67 @@ var lastStamp = null;
 function progress(item) {
   var logit = (item.__stamp !== lastStamp);
   if (logit) {
-    verbose('--iteration stamp:', [item.__user, item.__stamp]);
+    log('--iteration stamp:', [item.__user, item.__stamp]);
     lastStamp = item.__stamp;
   }
 }
 
-// batch's scope is dependant on how it is called
-// var batch = [];
-function bulkSave(batchSize) {
-  // verbose('--bulkSave',batchSize)
-  // save | bulk save[1] or bulk
-  if (!batchSize) {
-    return save;
-  }
-  // if (batchSize === 1) {
-  //   return function(item) {
-  //     db.bulkDocs([item]);
-  //   };
-  // }
-  // else
-  var batch = [];
-  return function(item) {
-      // verbose('-bulkSave:', [batchSize,batch.length,item._id]);
-    if (batch.length < batchSize) {
-      // verbose('-create:bulk:', [item._id,item._rev]);
-      batch.push(item);
-      // verbose('+bulkSave:', [batchSize,batch.length,item._id]);
-      return 'batched';
-    }
-    return db.bulkDocs(batch)
-      .then(function(result) {
-        verbose('create:bulk:', result);
-        batch = [];
-      });
-  };
+function createAndUpdate(credentials, stamp, file, item) {
+  log(item);
+  return create(item)
+    .then(save);
 }
 
-// just to create one only - preserves batch scope...
-var bulkSaver = bulkSave(100);
-function createAndUpdate(credentials, stamp, file, item) {
-  progress(item);
-  return create(item)
-    // .then(save);
-    // .then(bulkSave(0));
-    // .then(bulkSave(1));
-    // .then(bulkSave(10));
-    .then(bulkSaver);
+var accsByUserByType = {};
 
+function getAccumulator(item) {
+  var __user = item.__user;
+  var __type = item.__type;
+  // log('getAccumulator', __user, __type);
+  if (!accsByUserByType[__user]) {
+    accsByUserByType[__user] = {};
+  }
+  if (!accsByUserByType[__user][__type]) {
+    accsByUserByType[__user][__type] = new delta.AccumulatorByUuid();
+  }
+  return accsByUserByType[__user][__type];
+}
+
+function itemHandler(credentials, stamp, file, item) {
+  // progress(item);
+  var JoeRogan = '873e7420-042d-012e-f9a4-00163e1b201c';
+  if (item.podcast_uuid !== JoeRogan) {
+    return;
+  }
+  var acc = getAccumulator(item);
+  acc.merge(item);
+  // createAndUpdate(credentials,stamp,file,item);
 }
 
 function restoreFileToCouch() {
   var extra = '';
   // var extra = 'noredux'; // to switch to noredux..
-  return srcFile.iterator(extra, allCredentials, createAndUpdate)
+  return srcFile.iterator(extra, allCredentials, itemHandler)
     .then(function(counts) {
       Object.keys(counts).forEach(function(name) {
         var c = counts[name];
-        verbose('--' + extra + '-- ' + name, ' |stamps|:' + c.stamp + ' |f|:' + c.file + ' |p|:' + c.part);
+        log('--' + extra + '-- ' + name, ' |stamps|:' + c.stamp + ' |f|:' + c.file + ' |p|:' + c.part);
+      });
+    })
+    .then(function() {
+      _.forEach(accsByUserByType, function(types, __user) {
+        _.forEach(types, function(accByUUID, __type) {
+          log(' -accs', __user, __type, _.keys(accByUUID.accumulators).length);
+          _.forEach(accByUUID.accumulators, function(acc, uuid) {
+            log('  -acc', __user, __type, uuid, acc.meta);
+
+          });
+        });
       });
     });
 }
 
 resetDB()
   .then(restoreFileToCouch)
-  .then(showAll())
+  // .then(showAll())
   .catch(verboseErrorHandler(false));

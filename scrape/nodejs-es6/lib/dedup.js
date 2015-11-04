e02a4dc1f5242bea996132043f2a991063c5db30
Daniel Lauzon <daniel.lauzon@gmail.com>
2015-11-04 18:24:07 -0500

    Cleanup dedup task with refactored code

--- scrape/nodejs-es6/lib/dedup.js
+++ scrape/nodejs-es6/lib/dedup.js
@@ -5,11 +5,11 @@
 // copy original file (when delta>0) to <data>/noredux/byUserStamp
 // overwrite minimal changeset to <data>/byUserStamp
 
+// dependencies - core-public-internal
 var fs = require('fs');
 var path = require('path');
 var mkdirp = require('mkdirp');
 var Promise = require('bluebird');
-var crypto = require('crypto');
 var _ = require('lodash');
 var utils = require('./utils');
 var srcFile = require('./source/file');
@@ -36,8 +36,7 @@ function dedupTask(credentials) {
       // utils.logStamp('Starting:Dedup for ' + credentials.name);
       // console.log('-|stamps|', stamps.length);
 
-      var podcastHistory = new delta.AccumulatorByUuid();
-      var episodeHistory = new delta.AccumulatorByUuid();
+      var historyByType = new delta.AccumulatorByTypeByUuid();
 
       var partCount = 0;
       var fileCount = 0;
@@ -63,20 +62,12 @@ function dedupTask(credentials) {
 
                     // passed item is cloned, and normalized in accumulator
 
-                    var changeCount = 0;
-                    if (item.__type === 'episode') {
-                      // console.log('|episode|', thingsToMerge.length,file);
-                      changeCount += episodeHistory.merge(item);
-                    } else {
-                      // console.log('|podcasts|', thingsToMerge.length,file);
-                      changeCount += podcastHistory.merge(item);
-                    }
+                    var changeCount = historyByType.merge(item);
 
                     if (changeCount > 0) {
                       fileHasChanges = true;
                       redux.push(item);
                       // console.log('---|Δ|', changeCount, item.title);
-                      // TODO append to redux
                     } else {
                       dedupPartCount++;
                     }
@@ -101,32 +92,7 @@ function dedupTask(credentials) {
             });
         })
         .then(function(dontCare) {
-          function sortAndSave(outfile, history) {
-
-            // console.log('|' + outfile + '|=', _.size(history.accumulators));
-            // just write out the accumulators dictionary, it is the only attribute!
-            var sorted = _.sortBy(history.accumulators, function(item) {
-              // should this use sortByAll ? not in 2.4.2
-              // careful sorting by [__changeCount], compare by string when returning an array
-              // this sorts by a numerically
-              // _.sortBy([{a:1},{a:2},{a:3},{a:11},{a:12}],function(item){return item.a;});
-              // this sorts a lexicographically
-              // _.sortBy([{a:1,b:'a'},{a:2,b:'a'},{a:3,b:'a'},{a:11,b:'a'},{a:12,b:'a'}],function(item){return [item.a,item.b];})
-              // return [item.meta.__changeCount,item.meta.__lastUpdated, item.uuid];
-
-              // sort by lastUpdated,uuid (for uniqueness)
-              return [item.meta.__lastUpdated, item.uuid];
-            }).reverse();
-            var json = JSON.stringify(sorted, null, 2);
-
-            fs.writeFileSync(path.join(sinkFile.dataDirname, outfile), json);
-            utils.logStamp('md5(' + outfile + '):' + md5(json));
-          }
-          sortAndSave('podcast-history-' + credentials.name + '.json', podcastHistory);
-          sortAndSave('episode-history-' + credentials.name + '.json', episodeHistory);
-
-          // console.log('Done:dedup[%s] |f|: %d/%d  |p|: %d/%d', credentials.name, dedupFileCount, fileCount, dedupPartCount, partCount);
-          // utils.logStamp('Done:Dedup[' + credentials.name + '] |f|:' + fileCount + ' |p|:' + partCount);
+          historyByType.sortAndSave(credentials.name);
           return stamps;
         });
 
@@ -213,9 +179,4 @@ function filenameAtBase(file, basePartialPath) {
   } else {
     return path.join(dataDirname, basePartialPath, file);
   }
-}
-
-function md5(str) {
-  var hash = crypto.createHash('md5').update(str).digest('hex');
-  return hash;
 }

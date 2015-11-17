5f933449e20b44dcf07ef26c0427ee2328eea4b8
Daniel Lauzon <daniel.lauzon@gmail.com>
2015-11-17 11:44:41 -0500

    Using node-loggly directly in lib/logcheck

--- scrape/nodejs-es6/lib/logcheck.js
+++ scrape/nodejs-es6/lib/logcheck.js
@@ -3,10 +3,12 @@
 // This abstracts querying the loggly endpoint
 // this allows us to coordinate/and compare the logging output from many running clients
 
+// TODO(daneroo): Promisify
+
 // dependencies - core-public-internal
 var fs = require('fs');
 var winston = require('winston');
-var loggly = require('../node_modules/winston-loggly/node_modules/loggly');
+var loggly = require('loggly');
 var _ = require('lodash');
 var Table = require('cli-table');
 var log = require('./log');
@@ -18,51 +20,28 @@ exports = module.exports = {
 var config = JSON.parse(fs.readFileSync('credentials.loggly.json').toString());
 var client = loggly.createClient(config);
 
+// The search option can be parametrized later, as params to query()
+// but for now this has a single purpose.
+var searchOptions = {
+  query: 'tag:pocketscrape AND json.md5 AND json.file:history-*',
+  from: '-24h',
+  until: 'now',
+  size: 1500,
+};
+// This function uses the node-loggly module directly, instead of winston-loggly
+// This makes this module more independant.
 function queryLoggly(callback) {
-  client.search({
-      // fields:['message'],
-      query: 'md5',
-      from: '-24h',
-      until: 'now',
-      size: 1500,
-      // rows: 1 // no effect
-    })
+  client.search(searchOptions)
     .run(function(err, results) {
       if (err) {
-        console.log(err);
+        // the error object doesn't work with loggly, convert to string to send
+        log.error('logcheck.query: %s',err);
       } else {
-        // Inspect the result set
-        // results.events = results.events.length;
-        console.dir(results);
         log.verbose('logcheck.query', {
           found: results.events.length,
-          page: results.page,
           total_events: results.total_events
         });
         callback(results.events);
       }
     });
-
-}
-
-function queryWinstonLoggly(callback) {
-  var options = {
-    from: new Date() - 24 * 60 * 60 * 1000,
-    until: new Date(),
-    size: 200, // just to prove this works!, default is 50
-    order: 'desc',
-    query: 'tag:pocketscrape AND json.md5 json.file:history-*'
-  };
-  winston.query(options, function(err, results) {
-    if (err) {
-      throw err;
-    }
-    log.verbose('logcheck.query', {
-      found: results.loggly.events.length,
-      page: results.loggly.page,
-      total_events: results.loggly.total_events
-    });
-    callback(results.loggly.events);
-  });
-
 }

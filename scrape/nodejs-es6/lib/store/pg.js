2858885a0adb8e0fe59113883058a23daf93219f
Daniel Lauzon <daniel.lauzon@gmail.com>
2015-11-28 16:50:55 -0500

    Move the store.impl.pg.save

--- scrape/nodejs-es6/lib/store/pg.js
+++ scrape/nodejs-es6/lib/store/pg.js
@@ -3,7 +3,7 @@
 // pg implementation (save only)
 
 // dependencies - core-public-internal
-// var _ = require('lodash');
+var _ = require('lodash');
 var log = require('../log');
 // these might be moved or exposed
 var pgu = require('./pg-utils');
@@ -14,7 +14,7 @@ var pgu = require('./pg-utils');
 exports = module.exports = {
   // save: (item, opts) => {}, // returns (Promise)(status in insert,duplicate,error)
   // load: (opts, cb) => {} // foreach item, cb(item);
-  // save: save,
+  save: save,
   pgu: pgu, // temporary
   init: pgu.init, // setup the database pool, ddl...
   end: pgu.end
@@ -22,6 +22,64 @@ exports = module.exports = {
 
 // opts: {check:first?} => Promise(status)
 function save(item, opts) {
-  log.debug('pg:save saving item', item);
-  return Promise.resolve(true);
+  // log.debug('pg:save saving item', item.__stamp);
+  return checkThenSaveItem(item);
+  // return saveItem(item);
+}
+
+// implementation
+function checkThenSaveItem(item) {
+  return confirmIdentical(item)
+    .then(isIdentical => {
+      return isIdentical || saveItem(item);
+    });
+}
+
+// Save each item : problem, how do we traverse keys in an ordered way?
+function saveItem(item) {
+  function getFields(item) {
+    return [item.__user, item.__stamp, item.__type, item.uuid, item.__sourceType, item];
+  }
+
+  var fields = getFields(item);
+  // var key = fields.slice(0, -1).join('/');
+  // console.log('-save', key);
+
+  return pgu.insert('INSERT into items(__user,__stamp,__type,uuid,__sourceType,item) VALUES($1,$2,$3,$4,$5,$6)', fields)
+    .then(function(rowCount) {
+      if (rowCount !== 1) {
+        console.log('insert rowCount', rowCount);
+      }
+    })
+    .catch(function(err) {
+      // todo check that values are equal...
+      if (err.message.startsWith('duplicate key')) {
+        return confirmIdentical(item);
+      } else {
+        throw err;
+      }
+    });
+}
+
+function confirmIdentical(item) {
+  var keys = [item.__user, item.__stamp, item.__type, item.uuid, item.__sourceType];
+  // (__user, __stamp, __type, uuid, __sourceType)
+  var sql = 'select item from items where __user=$1 AND __stamp=$2 AND __type=$3 AND uuid=$4 AND __sourceType=$5';
+  return pgu.query(sql, keys)
+    .then(result => {
+      if (result.length === 0 || !result[0].item) {
+        return false;
+      }
+      var dbitem = result[0].item;
+      var isIdentical = _.isEqual(item, dbitem);
+      if (!isIdentical) {
+        log('Failed duplicate check', keys.join('/'));
+        log('-', item);
+        log('+', dbitem);
+
+      } else {
+        // log('Checked that duplicate is identical', keys.join('/'));
+      }
+      return isIdentical;
+    });
 }
--- scrape/nodejs-es6/restore-pg.js
+++ scrape/nodejs-es6/restore-pg.js
@@ -161,8 +161,7 @@ function loader() {
     checkUser(item);
 
     // save to database
-    return checkThenSaveItem(item);
-    // return saveItem(item);
+    return store.impl.pg.save(item);
   }
 
   return {
@@ -190,63 +189,6 @@ function accumulateItems(credentials) {
       log('Merged', rows.length);
       historyByType.sortAndSave(_user);
     });
-}
-
-function checkThenSaveItem(item) {
-  return confirmIdentical(item)
-    .then(isIdentical => {
-      return isIdentical || saveItem(item);
-    });
-}
-//
-// Save each item : problem, how do we traverse keys in an ordered way?
-function saveItem(item) {
-  function getFields(item) {
-    return [item.__user, item.__stamp, item.__type, item.uuid, item.__sourceType, item];
-  }
-
-  var fields = getFields(item);
-  // var key = fields.slice(0, -1).join('/');
-  // console.log('-save', key);
-
-  return insert('INSERT into items(__user,__stamp,__type,uuid,__sourceType,item) VALUES($1,$2,$3,$4,$5,$6)', fields)
-    .then(function(rowCount) {
-      if (rowCount !== 1) {
-        console.log('insert rowCount', rowCount);
-      }
-    })
-    .catch(function(err) {
-      // todo check that values are equal...
-      if (err.message.startsWith('duplicate key')) {
-        return confirmIdentical(item);
-      } else {
-        throw err;
-      }
-    });
-}
-
-function confirmIdentical(item) {
-  var keys = [item.__user, item.__stamp, item.__type, item.uuid, item.__sourceType];
-  // (__user, __stamp, __type, uuid, __sourceType)
-  var sql = 'select item from items where __user=$1 AND __stamp=$2 AND __type=$3 AND uuid=$4 AND __sourceType=$5';
-  return query(sql, keys)
-    .then(result => {
-      if (result.length === 0 || !result[0].item) {
-        return false;
-      }
-      var dbitem = result[0].item;
-      var isIdentical = _.isEqual(item, dbitem);
-      if (!isIdentical) {
-        log('Failed duplicate check', keys.join('/'));
-        log('-', item);
-        log('+', dbitem);
-
-      } else {
-        // log('Checked that duplicate is identical', keys.join('/'));
-      }
-      return isIdentical;
-    });
-  // return true;
 }
 
 // ************ Utilities

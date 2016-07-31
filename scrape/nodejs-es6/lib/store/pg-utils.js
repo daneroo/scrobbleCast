10569edce547174976d7beb5b9b30d004e8c5ded
Daniel Lauzon <daniel.lauzon@irdeto.com>
2016-07-30 20:44:44 -0400

    Refactoring pg

--- scrape/nodejs-es6/lib/store/pg-utils.js
+++ scrape/nodejs-es6/lib/store/pg-utils.js
@@ -7,7 +7,7 @@ var Promise = require('bluebird');
 // var _ = require('lodash');
 // Taken from postgress-bluebird
 var pgp = require('pg-promise')({
-  // init options
+  // init options ? move to config
   promiseLib: Promise
 });
 var log = require('../log');
@@ -26,17 +26,16 @@ var db = pgp(config);
 // Exported API
 exports = module.exports = {
   db: db,
+  getNamedParametersForItem:getNamedParametersForItem,
+  insertSQL: insertSQL,
+  // deprecated - use db?
   helpers: pgp.helpers,
   query: query, // (sql,values) => Promise
   insert: insert, // (sql,values) => Promise
+  // lifecycle
   init: init, // return Promise(bool?)
   end: end // drain the pool! Doesn't loook async (pg.end has no cb.)
 };
-
-function end() {
-  log.debug('pgu:end Closing connections, drain the pool!');
-  pgp.end();
-}
 
 // just return result.rows, untils we need otherwise
 function query(sql, values) {
@@ -52,10 +51,52 @@ function insert(sql, values) {
     .then(function () {
       return 1;
     });
-    // .catch(function (error) {
-    //   log.error('pgu:insert:error ', error);
-    //   return 0;
-    // });
+  // .catch(function (error) {
+  //   log.error('pgu:insert:error ', error);
+  //   return 0;
+  // });
+}
+
+// support for insertSQL, getNamedParametersForItem only initialized once
+const columns = [
+  '__user',
+  '__stamp',
+  '__type',
+  'uuid',
+  // no camelCase! __sourceType -> __sourcetype
+  '__sourcetype',
+  'item'
+];
+const columnSet = new pgp.helpers.ColumnSet(columns, { table: 'items' });
+
+// useful for generating an object for Named Parameter query arguments
+// let nmParams = getNamedParametersForItem(item)
+// db.any("select * from items where __user = ${__user} and uuid = ${uuid}",nmParams)
+function getNamedParametersForItem(item) {
+  return {
+    __user: item.__user,
+    __stamp: item.__stamp,
+    __type: item.__type,
+    uuid: item.uuid,
+    // no camelCase! __sourceType -> __sourcetype
+    __sourcetype: item.__sourceType,
+    item: item
+  };
+}
+
+// generates insert sql statement for one or many items
+// if items is not an array, assume it is a single item
+// uses pg-promise insert helper
+function insertSQL(items) {
+  let fields;
+  if (Array.isArray(items)) {
+    fields = items.map(getNamedParametersForItem);
+  } else {
+    let item = items; // just to clear on our intent
+    fields = getNamedParametersForItem(item);
+  }
+  // this returns a (possibly mulitple) insert query
+  return pgp.helpers.insert(fields, columnSet);
 }
 
 function ddlSilent(ddl) {
@@ -97,4 +138,9 @@ function init() {
       // throw new Error('Early exit');
     });
 
+}
+
+function end() {
+  log.debug('pgu:end Closing connections, drain the pool!');
+  pgp.end();
 }
--- scrape/nodejs-es6/lib/store/pg.js
+++ scrape/nodejs-es6/lib/store/pg.js
@@ -61,10 +61,8 @@ function save(item) {
   // return saveButVerifyIfDuplicate(item);
 
   // speed benchamarks with ~135k items, redone with pgp
-  // result full=> checkThenSaveItem, emtpy=>saveButVerifyIfDuplicate
+  // Conclusion full=> checkThenSaveItem, emtpy=>saveButVerifyIfDuplicate
 
-  // checkThenSaveItem:full          200 seconds : sum ok
-  // checkThenSaveItem:full          174 seconds : sum ok
   // checkThenSaveItem:full          162 seconds : sum ok
   // checkThenSaveItem:empty         423 seconds : sum ok
   // saveButVerifyIfDuplicate:full   266 seconds : sum ok
@@ -87,68 +85,13 @@ function remove(item) {
     });
 }
 
-const columns = [
-  '__user',
-  '__stamp',
-  '__type',
-  'uuid',
-  // no camelCase! __sourceType -> __sourcetype
-  '__sourcetype',
-  'item'
-];
-const columnSet = new pgu.helpers.ColumnSet(columns, { table: 'items' });
-
-// TODO(daneroo): figure out promised return value
+// TODO(daneroo): figure out return promised return value
 function saveAll(items) {
   if (items.length === 0) {
     return Promise.resolve(true);
   }
-  return Promise.each(items, (item) => save(item));
-
-  function getFields(item) {
-    return {
-      __user: item.__user,
-      __stamp: item.__stamp,
-      __type: item.__type,
-      uuid: item.uuid,
-      // no camelCase! __sourceType -> __sourcetype
-      __sourcetype: item.__sourceType,
-      item: item
-    };
-  }
-
-
-  // speed benchamarks with ~135k items, redone with helpers.insert (single)
-  // insert,confirmIdenticalOnDuplicate:empty  134 seconds : sum ok
-  // insert,confirmIdenticalOnDuplicate:full   260 seconds : sum ok
-
-  let single = false;
-  if (single) {
-    return Promise.each(items, (item) => {
-      // Column details can be taken from the data object:
-
-      let fields = getFields(item);
-
-      // let sql = pgu.helpers.insert(fields, null, 'items');
-      let sql = pgu.helpers.insert(fields, columnSet);
-
-      // log.verbose('sql', sql);
-      return pgu.db.none(sql)
-        // .then(() => {
-        //   // log.verbose('insert ok');
-        // })
-        .catch((error) => {
-          if (error.message.startsWith('duplicate key')) {
-            // log.verbose('duplicate')
-            //  checkThenSaveItem would be same...
-            return confirmIdentical(item);
-          } else {
-            log.verbose('insert error', error);
-            throw error;
-          }
-        });
-    });
-  }
+  // Bruteforce implementation:  items.each save!
+  // return Promise.each(items, (item) => save(item));
 
   // speed benchamarks with ~135k items, redone with helpers.insert (multi)
   // batch=2 insert,each.save:empty     85 seconds : sum ok
@@ -161,48 +104,34 @@ function saveAll(items) {
   // batch=1000 insert,each.save:half  111 seconds : sum ok
   // batch=1000 insert,each.save:half  113 seconds : sum ok
 
+  // batch=1000 insert,each.save:full  179 seconds : sum ok
+
   // TODO(daneroo): precede with filtering not present by hash!
   // which would turn this into sync!
   // select digest from items where digest in [digest_0,...digest_batchsize]
 
-  let multiple = true;
-  if (multiple) {
-    // log.verbose('multi', items.length);
+  return pgu.db.none(pgu.insertSQL(items))
+    .catch((error) => {
+      if (error.message.startsWith('duplicate key')) {
+        // log.verbose('at least one duplicate');
+        // at least one duplicate, means the entire statement failed!
+        // fall back to iterating on each item.save
+        // adjust return value on success
+        return Promise.each(items, (item) => save(item))
+          .then(() => true);
 
-    let fields = items.map(getFields);
-    // log.verbose('multi.fields', fields);
-    let sql = pgu.helpers.insert(fields, columnSet);
-    // log.verbose('multi.sql', sql);
-    return pgu.db.none(sql)
-      // .then(() => {
-      //   // log.verbose('insert ok');
-      // })
-      .catch((error) => {
-        if (error.message.startsWith('duplicate key')) {
-          // log.verbose('at least one duplicate');
-          return Promise.each(items, (item) => save(item))
-            .then(() => {
-              // log.verbose('re-inserted on by one, done');
-              return true;
-            });
-        } else {
-          log.verbose('insert error', error);
-          throw error;
-        }
-      });
+      } else {
+        log.verbose('insert error', error);
+        throw error;
+      }
+    });
 
-  }
 
 }
 
 //TODO(daneroo) Right now, if confirmIdentical is false, but key is present, return false, but should throw!
 // implementations
 function checkThenSaveItem(item) {
-  // return confirmIdenticalByDigestCount(item)
-  // return confirmIdentical(item)
-  //   .then(isIdentical => {
-  //     return isIdentical || saveItem(item);
-  //   });
   return confirmIdenticalByDigest(item)
     .then(isIdentical => {
       return isIdentical || confirmIdentical(item);
@@ -226,20 +155,7 @@ function saveButVerifyIfDuplicate(item) {
 
 // Save each item : problem, how do we traverse keys in an ordered way?
 function saveItem(item) {
-  function getFields(item) {
-    return [item.__user, item.__stamp, item.__type, item.uuid, item.__sourceType, item];
-  }
-
-  var fields = getFields(item);
-  // var key = fields.slice(0, -1).join('/');
-  // console.log('-save', key);
-
-  return pgu.insert('INSERT into items(__user,__stamp,__type,uuid,__sourceType,item) VALUES($1,$2,$3,$4,$5,$6)', fields)
-    .then(function (rowCount) {
-      if (rowCount !== 1) {
-        // console.log('insert rowCount', rowCount);
-      }
-    });
+  return pgu.db.none(pgu.insertSQL(item));
 }
 
 function confirmIdentical(item) {

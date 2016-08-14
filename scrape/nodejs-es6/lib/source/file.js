273306f6ff833eb0394f81e0d2a267a1aff66ab5
Daniel Lauzon <daniel.lauzon@irdeto.com>
2016-08-14 14:31:43 -0400

    eslint: lib/source/file.js

--- scrape/nodejs-es6/lib/source/file.js
+++ scrape/nodejs-es6/lib/source/file.js
@@ -31,7 +31,7 @@ function loadJSON(file) {
 function confirmSorted(files) {
   var sorted = true;
   var lastFile;
-  files.forEach(function(file) {
+  files.forEach(function (file) {
     if (lastFile) {
       var ok = file >= lastFile;
       if (!ok) {
@@ -58,7 +58,7 @@ function findByUserStamp(user, basepath) {
   var dir = path.join(basepath, 'byUserStamp', user);
   return fs.readdirPromise(dir)
     .then(confirmSorted)
-    .catch(function(err) {
+    .catch(function (err) {
       // log and rethrow
       console.log('findByUserStamp error:', err);
       throw err;
@@ -68,15 +68,15 @@ function findByUserStamp(user, basepath) {
 //  just break this into parts by Date
 function find(pattern) {
   return globPromise(pattern, {
-      cwd: dataDirname
-    })
-    .then(function(files) {
+    cwd: dataDirname
+  })
+    .then(function (files) {
       // just for debugging
       // console.log('globPromise %s found: %d files', pattern, files.length);
       return files;
     })
     .then(confirmSorted)
-    .catch(function(err) {
+    .catch(function (err) {
       // log and rethrow
       console.log('globPromise error:', err);
       throw err;
@@ -94,38 +94,38 @@ function iterator(extrapath, allCredentials, callbackReturningPromise, pattern, 
   pattern = pattern || '**/*.json';
   var basepath = path.join(dataDirname, extrapath);
   var counts = {};
-  return Promise.each(allCredentials, function(credentials) {
-      counts[credentials.name] = counts[credentials.name] || {
-        part: 0,
-        file: 0,
-        stamp: 0,
-        ignoredFiles: 0
-      };
-      var c = counts[credentials.name];
-      return findByUserStamp(credentials.name, basepath)
-        .then(function(stamps) {
-          return Promise.each(stamps, function(stamp) {
-            return find(path.join(extrapath, 'byUserStamp', credentials.name, stamp, pattern))
-              .then(function(files) {
-                c.stamp++;
-                return Promise.each(files, function(file) {
-                  if (fileFilter && !fileFilter(credentials, stamp, file)) {
-                    c.ignoredFiles++;
-                    return Promise.resolve(true);
-                  }
-                  var items = loadJSON(file);
-                  c.file++;
-                  return Promise.each(items, function(item) {
-                    c.part++;
-                    return callbackReturningPromise(credentials, stamp, file, item, counts);
-                  });
-
+  return Promise.each(allCredentials, function (credentials) {
+    counts[credentials.name] = counts[credentials.name] || {
+      part: 0,
+      file: 0,
+      stamp: 0,
+      ignoredFiles: 0
+    };
+    var c = counts[credentials.name];
+    return findByUserStamp(credentials.name, basepath)
+      .then(function (stamps) {
+        return Promise.each(stamps, function (stamp) {
+          return find(path.join(extrapath, 'byUserStamp', credentials.name, stamp, pattern))
+            .then(function (files) {
+              c.stamp++;
+              return Promise.each(files, function (file) {
+                if (fileFilter && !fileFilter(credentials, stamp, file)) {
+                  c.ignoredFiles++;
+                  return Promise.resolve(true);
+                }
+                var items = loadJSON(file);
+                c.file++;
+                return Promise.each(items, function (item) {
+                  c.part++;
+                  return callbackReturningPromise(credentials, stamp, file, item, counts);
                 });
+
               });
-          });
+            });
         });
-    })
-    .then(function() {
+      });
+  })
+    .then(function () {
       return counts;
     });
 }
@@ -148,7 +148,7 @@ function iteratorWithRollup(extrapath, allCredentials, callbackReturningPromise,
   // For the second part:
   // compose a dateSkippFilter with any passed in fileFilter (if present)
   // our date filter takes precedence
-  function skippingWrappedFilter(credentials, stamp, file, item) {
+  function skippingWrappedFilter(credentials, stamp, file /*, item */) {
     var shouldProceed = (stamp > maxStamp);
     if (!shouldProceed) {
       return shouldProceed;
@@ -159,7 +159,7 @@ function iteratorWithRollup(extrapath, allCredentials, callbackReturningPromise,
   }
 
   return iterator('rollup', allCredentials, wrapCallbackAndGrabDateHandler, pattern, fileFilter)
-    .then(function(counts) {
+    .then(function (/*counts*/) {
       // TODO correct return counts...
       console.log('Now call iterator with extrapath: %s after: %s', extrapath, maxStamp);
       return iterator(extrapath, allCredentials, callbackReturningPromise, pattern, skippingWrappedFilter);

ec0254ec3685799fce553de2cd7b73208234eedf
Daniel Lauzon <daniel.lauzon@gmail.com>
2021-03-14 17:31:20 -0400

    Replace Header and Sub-header

--- site/pages/_app.js
+++ site/pages/_app.js
@@ -1,7 +1,7 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import theme from '../theme'
 
-function MyApp({ Component, pageProps }) {
+function MyApp ({ Component, pageProps }) {
   return (
     <ChakraProvider resetCSS theme={theme}>
       <Component {...pageProps} />

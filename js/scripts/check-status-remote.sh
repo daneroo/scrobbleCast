2bf12882c7c5348af3f8a23064e510497e1b6fd0
Daniel Lauzon <daniel.lauzon@gmail.com>
2024-11-25 18:30:11 -0500

    update var to use common HOSTS

--- js/scripts/check-status-remote.sh
+++ js/scripts/check-status-remote.sh
@@ -3,26 +3,23 @@
 # Source common functions and variables
 source "$(dirname "$0")/common.sh"
 
-# Define hosts array
-hosts=("darwin" "dirac" "d1-px1")
-
 # Check SSH key first
 check_ssh_key || exit 1
 
 format "## Checking versions"
-for host in "${hosts[@]}"; do
+for host in "${HOSTS[@]}"; do
     version=$(curl -s "http://${host}.imetrical.com:8000/api/version")
     echo "${host} ${version}"
 done
 
 format "## Checking status"
-for host in "${hosts[@]}"; do
+for host in "${HOSTS[@]}"; do
     status=$(curl -s "http://${host}.imetrical.com:8000/api/status")
     echo "${host} ${status}"
 done
 
 format "## Checking digests"
-for host in "${hosts[@]}"; do
+for host in "${HOSTS[@]}"; do
     digest=$(curl -s "http://${host}.imetrical.com:8000/api/digests" | shasum -a 256)
     echo "${host} ${digest}"
 done
